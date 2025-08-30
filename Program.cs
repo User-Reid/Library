ddb740aa513905dde9c7904f5a7b5a883fca250b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-30 14:32:24 -0500

    mucho progresso

--- Program.cs
+++ Program.cs
@@ -1,2 +1,133 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+bool applicationStatus = true;
+var library = new LibraryArchive();
+string filePath = "libraryArchive.json";
+if (!File.Exists("libraryArchive.json"))
+{
+  File.WriteAllText("libraryArchive.json", "[]");
+}
+
+App.MainMenu(applicationStatus, filePath, library.Books);
+
+
+System.Console.WriteLine("Application has closed, please press any button to exit.");
+Console.ReadKey();
+
+public class App
+{
+  public static void MainMenu(bool applicationStatus, string filePath, List<Book> library)
+  {
+    System.Console.WriteLine("What would you like to do?");
+    System.Console.WriteLine("Press [A] to add to your library");
+    System.Console.WriteLine("Press [R] to read your library");
+    System.Console.WriteLine("Press [D] to delete from your library");
+    System.Console.WriteLine("Press [E] to exit the Application");
+    var userInput = Console.ReadLine();
+    char userInputCharacter = char.Parse(userInput);
+
+    switch (userInputCharacter)
+    {
+      case 'A':
+      case 'a':
+        AddToLibrary.Add(filePath, library);
+        break;
+      case 'R':
+      case 'r':
+        ReadLibrary.Read(filePath, library);
+        break;
+      case 'D':
+      case 'd':
+        DeletionFromLibrary.DeleteFromLibrary(filePath, library);
+        break;
+      // case 'E':
+      // case 'e':
+      //   Exit();
+      //   break;
+      default:
+        {
+          applicationStatus = false;
+        }
+        break;
+    }
+  }
+}
+
+public class LibraryArchive
+{
+  public List<Book> Books { get; } = new List<Book>();
+}
+
+public class Book
+{
+  public string Title { get; }
+  public string Author { get; }
+  public int ReleaseYear { get; }
+  public decimal Rating { get; }
+
+  public Book(string title, string author, int releaseYear, decimal rating)
+  {
+    Title = title;
+    Author = author;
+    ReleaseYear = releaseYear;
+    Rating = rating;
+  }
+
+  public override string ToString() => $"{Title} written by {Author}\r\n{ReleaseYear} {Rating}/5";
+}
+
+public static class ReadLibrary
+{
+  public static List<Book> Read(string filePath, List<Book> library)
+  {
+    List<Book> updatedLibrary = library;
+    var fileContents = File.ReadAllText(filePath);
+    List<Book> jsonLibrary = JsonSerializer.Deserialize<List<Book>>(fileContents);
+    foreach (Book book in jsonLibrary)
+    {
+      System.Console.WriteLine(book);
+    }
+    return updatedLibrary;
+  }
+}
+
+public static class AddToLibrary
+{
+  public static void Add(string filePath, List<Book> library)
+  {
+    System.Console.WriteLine("What is the title of the book you would like to add?📘");
+    string title = Console.ReadLine();
+    System.Console.WriteLine("Who was the author?✍️");
+    string author = Console.ReadLine();
+    System.Console.WriteLine("What year was it released?⌛");
+    int releaseYear = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("What would you rate it 1 - 5?⭐");
+    decimal rating = decimal.Parse(Console.ReadLine());
+
+    Book newBook = new Book(title, author, releaseYear, rating);
+    library.Add(newBook);
+    System.Console.WriteLine($"{newBook.Title} has been added to your library 😄");
+
+  }
+}
+
+public static class DeletionFromLibrary
+{
+  public static List<Book> DeleteFromLibrary(string filePath, List<Book> library)
+  {
+    List<Book> updatedLibrary = library;
+    System.Console.WriteLine("Which book would you like to remove from your library?");
+    ReadLibrary.Read(filePath, library);
+    System.Console.WriteLine();
+    var userInput = Console.ReadLine();
+
+    foreach (Book book in library)
+    {
+      if (userInput == book.Title)
+      {
+        library.Remove(book);
+      }
+    }
+    return updatedLibrary;
+  }
+}
